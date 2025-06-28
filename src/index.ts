c210b81860775c86bac248349155faf92213bd9e
prashant-m4 <prashantsingh090798@gmail.com>
2025-06-28 11:59:38 +0530

    feat: primary index using map : O(n) in worst case of on to many relationship (when colum is not unique)

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,31 @@
-import { Users, User } from "./data"
+import { Users, User } from "./data";
 
-const buildPrimaryIndex = (Users: User[]) => {
+const buildPrimaryIndex = (Users: User[], key: keyof User) => {
+    const primaryIndex: Record<string, number[]> = {};
 
-}
+    Users.forEach((user, index) => {
+        const k = String(user[key]);
+        if (!primaryIndex[k]) {
+            primaryIndex[k] = [];
+        }
+        primaryIndex[k].push(index);
+    });
+    console.log(primaryIndex)
+
+    return primaryIndex;
+};
+
+const primaryIndexOnUsersByName = buildPrimaryIndex(Users, "name");
 
 const getUsersByName = (name: string) => {
-    const users = Users.filter((user)=> user.name === name)
+    const recordPointers = primaryIndexOnUsersByName[name] || [];
+    const users = recordPointers.map((index) => Users[index]);
     return users;
-}
+};
 
 const main = () => {
     const users = getUsersByName("Prashant");
-    console.log("users",users);
-}
+    console.log("users", users);
+};
 
 main();
